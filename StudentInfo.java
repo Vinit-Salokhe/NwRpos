9a63af0843c311d80258dd7e24191f4eb60a6473
Vinit-Salokhe <vinitsalokhe3540@gmail.com>
2021-03-06 22:54:36 +0530

    My first commit

---
+++ StudentInfo.java
@@ -0,0 +1,38 @@
+package com.operator;
+
+public class StudentInfo {
+
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 20;
+		
+		{	int c;
+		c = a+b;
+		System.out.println("Addition is: "+c);}
+		
+		{int c;
+		c = a-b;
+		System.out.println("Subtraction is: "+c);}
+		
+		{int c;
+		c = a*b;
+		System.out.println("Multiplicaton is: "+c);}
+		
+		{int c;
+		c = a%b;
+		System.out.println("Modulo is: "+c);}
+		
+		{int c;
+		c = a/b;
+		System.out.println("Divation is: "+c);}
+	}
+		
+		
+		
+
+	}
+
